221ad1c34b65105803ed2f5127021909d72c7430
Sarah[SSM] <55794570+SarahSSM131@users.noreply.github.com>
2022-01-09 17:44:34 +0100

    Set localization settings + front of first interface (#3)

--- app/src/main/java/com/example/weather_project/view/MainActivity.kt
+++ app/src/main/java/com/example/weather_project/view/MainActivity.kt
@@ -1,12 +1,72 @@
 package com.example.weather_project.view
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
 import com.example.weather_project.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val LOCAL_PERMISSION_ID = 1010
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainBtnId.setOnClickListener {
+            Log.d("Debug:",CheckPermission().toString())
+            Log.d("Debug:",isLocationEnabled().toString())
+            RequestPermission()
+        }
+        window.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+        //Launch SignIn Activity in the Main One (changer les noms à voir)
+        mainSignInId.setOnClickListener {
+            val signintempIntent = Intent (this, SignInActivity::class.java)
+            startActivity(signintempIntent)
+        }
+    }
+
+    //Check of permissions (in the manifest, by the users and following the settings)
+    fun CheckPermission():Boolean{
+        if(
+            ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ){
+            //Launch WeatherMenu in the Main One
+            val weathertempIntent = Intent(this, Weather_Display::class.java)
+            startActivity(weathertempIntent)
+            return true
+        }
+        return false
+    }
+
+    fun RequestPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),
+            LOCAL_PERMISSION_ID
+        )
+    }
+
+    fun isLocationEnabled():Boolean{
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 }
+
+
