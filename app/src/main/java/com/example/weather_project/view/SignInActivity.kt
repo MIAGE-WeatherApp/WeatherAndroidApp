a8418a39ed4189c0d2c9cd86a2395ff869b3efee
SarahSSM131 <sarah.marecar@gmail.com>
2022-01-26 21:46:24 +0100

    Add features for the project because I am the BOSS :)

--- app/src/main/java/com/example/weather_project/view/SignInActivity.kt
+++ app/src/main/java/com/example/weather_project/view/SignInActivity.kt
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weather_project.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        LogSignupId.setOnClickListener{
+            val signup_intent = Intent(this, SignupActivity::class.java)
+            startActivity(signup_intent)
+        }
     }
 }
